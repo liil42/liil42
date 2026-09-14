e88d5260683082fd3a1340c60489055690119000
liil42 <2239339031@qq.com>
2026-09-14 22:09:27 +0800

    chore: sync deploy changes

---
+++ work-encoding-report.js
@@ -0,0 +1,21 @@
+﻿const fs = require('fs');
+const path = require('path');
+const dirs = ['client/src', 'server/src'];
+const report = [];
+for (const dir of dirs) {
+  const walk = (d) => {
+    for (const entry of fs.readdirSync(d, { withFileTypes: true })) {
+      const p = path.join(d, entry.name);
+      if (entry.isDirectory()) walk(p);
+      else if (/\.(js|jsx|css|json)$/i.test(entry.name)) {
+        const buf = fs.readFileSync(p);
+        let replacement = 0;
+        let invalid = 0;
+        for (const b of buf) if (b >= 0x80) invalid += 1;
+        report.push({ file: p, bytes: buf.length, highBytes: invalid, replacement });
+      }
+    }
+  };
+  walk(dir);
+}
+for (const item of report) console.log(`${item.highBytes}\t${item.file}`);
---
+++ work-fix-tutorial-paths.js
@@ -0,0 +1,7 @@
+﻿const fs = require('fs');
+const p = 'client/public/tutorial/api-key.html';
+let s = fs.readFileSync(p, 'utf8');
+s = s.replace(/href="\/"/g, 'href="../"');
+s = s.replace(/src="\/tutorial\//g, 'src="./');
+fs.writeFileSync(p, s, 'utf8');
+console.log(s.match(/(?:href|src)="[^"]+"/g).join('\n'));
