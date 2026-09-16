471375f08cf70d7bec14bb3fae24d28461b070af
liil42 <2239339031@qq.com>
2026-09-17 07:38:54 +0800

    deploy: update GitHub Pages docs to Railway backend build

--- docs/assets/swimlanesDiagram-VR7AAH4N-Bd_DuIYu.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-HODETNUW-DE9VPOlG.js";import{_ as o}from"./mermaid.core-BfvYNPl5.js";import"./chunk-5VM5RSS4-CYzoWaYI.js";import"./chunk-XXDRQBXY-BkRVz_fO.js";import"./chunk-POPQ4Y6H-Bem0n2IN.js";import"./chunk-F27PBJKO-m1HeUy0H.js";import"./channel-DpVKAR_u.js";import"./index-DeOKQos6.js";import"./markdown-CnjyDuPh.js";import"./icons-BmbRJ3q8.js";import"./react-WhCeIeUH.js";import"./zip-DuqutW8b.js";var a=o(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),m=a,v=r({defaultLayout:"swimlane",styles:m});export{v as diagram};
